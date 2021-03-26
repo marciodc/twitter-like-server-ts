7d41fc98e242c3efeef022b74b09a6f36caba549
Marcio D. Carvalho <marciodc@gmail.com>
2021-03-26 09:11:38 -0300

    Database

--- src/database/database.ts
+++ src/database/database.ts
@@ -9,25 +9,42 @@ export class Database {
   }
 
   init(): void {
-    this.connection = new Sequelize({
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      database: process.env.DB_NAME,
-      dialect: 'postgres',
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      logging: msg => { console.log(msg) },
-      define: {
-        freezeTableName: true,
-      }// ,
-      // dialectOptions: {
-      //   ssl: {
-      //     require: true,
-      //     rejectUnauthorized: false
-      //   }
-      // }
-    })
+    if (process.env.DB_HOST == 'localhost') {
+      this.connection = new Sequelize({
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        dialect: 'postgres',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        logging: msg => { console.log(msg) },
+        define: {
+          freezeTableName: true,
+        }
+      })
 
-    this.connection.addModels([User, Message, Token, Following]);
+      this.connection.addModels([User, Message, Token, Following])
+    } else {
+      this.connection = new Sequelize({
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        dialect: 'postgres',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        logging: msg => { console.log(msg) },
+        define: {
+          freezeTableName: true,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      })
+  
+      this.connection.addModels([User, Message, Token, Following])
+    }
   }
 }
